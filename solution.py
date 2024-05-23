82901f6200016939822a3f3d25f98eff8d42c4e3
Timofey1317 <105978651+Timofey1317@users.noreply.github.com>
2024-05-23 15:56:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
+chat_id = 1028099632 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(c_npv: np.array, t_npv: np.array) -> bool:# Одна или две выборке на входе, заполняется исходя из условия
+    
+    alpha = 0.08
+    
+    _, p_value_c = stats.shapiro(c_npv)
+    _, p_value_t = stats.shapiro(t_npv)
+    
+    if p_value_c > alpha and p_value_t > alpha:
+        statistic, p_value = stats.ttest_ind(c_npv, t_npv, equal_var=False)
+    else:
+        statistic, p_value = stats.mannwhitneyu(c_npv, t_npv, alternative='two-sided')
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return p_value < alpha # Ваш ответ, True или False
